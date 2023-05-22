568687ef897adfe8dd25e63562a6fcf8cb335901
Vitaliy <zangedsu@gmail.com>
2023-05-22 13:19:54 +0300

    Добавли метод и событие на получение обновлений

--- TelegramCheker/Program.cs
+++ TelegramCheker/Program.cs
@@ -1,4 +1,7 @@
 ﻿// ручная конфигурация
+using TL;
+using WTelegram;
+
 static string Config(string what)
 {
     switch (what)
@@ -20,13 +23,19 @@ using var client = new WTelegram.Client(Config);
 var myself = await client.LoginUserIfNeeded();
 Console.WriteLine($"We are logged-in as {myself} (id {myself.id})");
 
+ Dictionary<long, User> Users = new();
+ Dictionary<long, ChatBase> Chats = new();
 
+client.OnUpdate += Client_OnUpdate;
 
-// пример использования
+// пример использования 
+
 // получаем список личных диалогов
 var dialogs = await client.Messages_GetAllDialogs();
 // получаем список чатов и каналов
 var chats = await client.Messages_GetAllChats();
+
+dialogs.CollectUsersChats(Users, Chats);
 
 //выводим список личных диалогов
 Console.WriteLine("Список личных диалогов:");
@@ -40,6 +49,7 @@ foreach (var (id, chat) in chats.chats)
     if (chat.IsActive)
         Console.WriteLine($"{id,10}: {chat}");
 
+Console.ReadKey();
 // отправка сообщения в чат или канал
 //Console.Write("Type a chat ID to send a message: ");
 //long chatId = long.Parse(Console.ReadLine());
@@ -48,8 +58,49 @@ foreach (var (id, chat) in chats.chats)
 //await client.SendMessageAsync(target, "Hello, World");
 
 // отправка личного сообщения
-Console.Write("Type a chat ID to send a message: ");
-long chatId = long.Parse(Console.ReadLine());
-var target = dialogs.users[chatId];
-Console.WriteLine($"Sending a message in chat {chatId}: {target.MainUsername}");
-await client.SendMessageAsync(target, "Hello, World");
+//Console.Write("Type a chat ID to send a message: ");
+//long chatId = long.Parse(Console.ReadLine());
+//var target = dialogs.users[chatId];
+//Console.WriteLine($"Sending a message in chat {chatId}: {target.MainUsername}");
+//await client.SendMessageAsync(target, "Hello, World");
+
+
+
+// апдейты
+  async Task Client_OnUpdate(UpdatesBase updates)
+{
+    updates.CollectUsersChats(Users, Chats);
+    foreach (var update in updates.UpdateList)
+        switch (update)
+        {
+            case UpdateNewMessage unm: await DisplayMessage(unm.message); break;
+            case UpdateEditMessage uem: await DisplayMessage(uem.message, true); break;
+            // Note: UpdateNewChannelMessage and UpdateEditChannelMessage are also handled by above cases
+            case UpdateDeleteChannelMessages udcm: Console.WriteLine($"{udcm.messages.Length} message(s) deleted in {Chat(udcm.channel_id)}"); break;
+            case UpdateDeleteMessages udm: Console.WriteLine($"{udm.messages.Length} message(s) deleted"); break;
+            case UpdateUserTyping uut: Console.WriteLine($"{User(uut.user_id)} is {uut.action}"); break;
+            case UpdateChatUserTyping ucut: Console.WriteLine($"{Peer(ucut.from_id)} is {ucut.action} in {Chat(ucut.chat_id)}"); break;
+            case UpdateChannelUserTyping ucut2: Console.WriteLine($"{Peer(ucut2.from_id)} is {ucut2.action} in {Chat(ucut2.channel_id)}"); break;
+            case UpdateChatParticipants { participants: ChatParticipants cp }: Console.WriteLine($"{cp.participants.Length} participants in {Chat(cp.chat_id)}"); break;
+            case UpdateUserStatus uus: Console.WriteLine($"{User(uus.user_id)} is now {uus.status.GetType().Name[10..]}"); break;
+            case UpdateUserName uun: Console.WriteLine($"{User(uun.user_id)} has changed profile name: {uun.first_name} {uun.last_name}"); break;
+            case UpdateUser uu: Console.WriteLine($"{User(uu.user_id)} has changed infos/photo"); break;
+            default: Console.WriteLine(update.GetType().Name); break; // there are much more update types than the above example cases
+        }
+}
+
+  Task DisplayMessage(MessageBase messageBase, bool edit = false)
+{
+    if (edit) Console.Write("(Edit): ");
+    switch (messageBase)
+    {
+        case Message m: Console.WriteLine($"{Peer(m.from_id) ?? m.post_author} in {Peer(m.peer_id)}> {m.message}"); break;
+        case MessageService ms: Console.WriteLine($"{Peer(ms.from_id)} in {Peer(ms.peer_id)} [{ms.action.GetType().Name[13..]}]"); break;
+    }
+    return Task.CompletedTask;
+}
+
+ string User(long id) => Users.TryGetValue(id, out var user) ? user.ToString() : $"User {id}";
+ string Chat(long id) => Chats.TryGetValue(id, out var chat) ? chat.ToString() : $"Chat {id}";
+ string Peer(Peer peer) => peer is null ? null : peer is PeerUser user ? User(user.user_id)
+    : peer is PeerChat or PeerChannel ? Chat(peer.ID) : $"Peer {peer.ID}";
