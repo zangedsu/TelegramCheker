e7f04fe77dca1d6965fc6e6066161b7997c715f6
Vitaliy <zangedsu@gmail.com>
2023-05-23 12:20:34 +0300

    upd

--- TelegramCheker/Models/Config.cs
+++ TelegramCheker/Models/Config.cs
@@ -7,19 +7,22 @@ using System.Threading.Tasks;
 namespace TelegramCheker.Models;
     internal class Config
     {
-    public long TargetChatId { get; set; } = 12345678;
-    public string LoginPhoneNumber { get; set; } = "234234123423";
+    public long TargetChatId { get; set; }
+    public string LoginPhoneNumber { get; set; }
     public string? Password { get; set; }
     public long OutputChatId { get; set; }
+    public string FirstMessage { get; set; }
 
-    public Config(long targetChatId, string loginPhoneNumber, string? password, long outputChatId)
+    public Config(long targetChatId, string loginPhoneNumber, string? password, long outputChatId, string firstMessage)
     {
         TargetChatId = targetChatId;
         LoginPhoneNumber = loginPhoneNumber;
         Password = password;
         OutputChatId = outputChatId;
+        FirstMessage = firstMessage;
     }
 
-    public Config() : this (1234567, "YOUR_PHONE_HERE", "2FaPassword(if need)", 123434){}
+    public Config() : this (1234567, "YOUR_PHONE_HERE", "2FaPassword(if need)", 123434, 
+        "Здравствуйте. Вы искали специалиста по контекстной рекламе. Подскажите, еще данный вопрос актуален?") {}
 }
 
