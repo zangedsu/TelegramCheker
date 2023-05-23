e7bb6681716d20237837e2d0d6caac9c380a5a0e
Vitaliy <zangedsu@gmail.com>
2023-05-23 12:37:27 +0300

    Реализовал класс Subject

--- TelegramCheker/Models/Subject.cs
+++ TelegramCheker/Models/Subject.cs
@@ -9,6 +9,24 @@ namespace TelegramCheker.Models;
     internal class Subject
     {
     // TODO: Реализовать модель, описывающую проверяемого пользователя
+    public long UserId { get; set; }
+    public bool IsTestsPassed { get; set; }
+    public bool IsOnCheckNow { get; set; }
+    public DateTime FirstMessageSentAt { get; set; }
 
+    /// <summary>
+    /// Класс, описывающий проверяемого пользователя
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="isTestPassed"></param>
+    /// <param name="isOnCheckNow"></param>
+    /// <param name="firstMessageSentAt"></param>
+    public Subject(long userId, bool isTestPassed, bool isOnCheckNow, DateTime firstMessageSentAt)
+    {
+        UserId = userId;
+        IsTestsPassed = isTestPassed;
+        IsOnCheckNow = isOnCheckNow;
+        FirstMessageSentAt = firstMessageSentAt;
     }
+}
 
