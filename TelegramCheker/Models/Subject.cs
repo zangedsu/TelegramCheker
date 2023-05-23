bd72446ab7e249902a1055eedfd75d0ebae311ef
Vitaliy <zangedsu@gmail.com>
2023-05-23 12:24:03 +0300

    Add Subject -> Models

---
+++ TelegramCheker/Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramCheker.Models;
+
+    internal class Subject
+    {
+    // TODO: Реализовать модель, описывающую проверяемого пользователя
+
+    }
+
