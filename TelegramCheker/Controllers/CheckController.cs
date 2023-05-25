19ef60215d3ec548e8200e769adbd958e1b95b41
Vitaliy <zangedsu@gmail.com>
2023-05-25 17:47:38 +0300

    Добавил отправку сообщения новым пользователям

--- TelegramCheker/Controllers/CheckController.cs
+++ TelegramCheker/Controllers/CheckController.cs
@@ -20,13 +20,40 @@ namespace TelegramCheker.Controllers;
 
     public async void recievedNewMessageFromTChat(string message, string username, WTelegram.Client client)
     {
-        var resolved = await client.Contacts_ResolveUsername("FDV_photo"); // username without the @
-        await client.SendMessageAsync(resolved, _data.ProgramConfig.FirstMessage);
+      username = username.Trim('@');
+        int index = -1;
 
-       // var tmp = from s in _subjects
-       //           from n in s.UserName
+        for (int i = 0; i < _subjects.Count; i++)
+        {
+            if (_subjects[i].UserName == username)
+            {
+                //если пользователь уже есть в базе
+                // TODO: произвести обработку (проверку)
+                index = i; break;
+            }
+        }
+
+        if(index == -1)
+        {
+            Thread.Sleep(1000);
+            _subjects.Add(new(username));
+            sendFirstMessage(username, client);
+        }
+        else
+        {
+            // TODO: реализовать проверку ответа пользователя
+        }
+
 
     }
+
+    // отправка первого сообщеня
+    private async void sendFirstMessage(string username, WTelegram.Client client)
+     {
+        var resolved = await client.Contacts_ResolveUsername(username); // username without the @
+        await client.SendMessageAsync(resolved, _data.ProgramConfig.FirstMessage);
+        Console.WriteLine("Отправили первое сообщение в чат с " + "@" + username);
+     }
   
     }
 
